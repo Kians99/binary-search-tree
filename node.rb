48c6d1eb5ca1830965fd2e8c7e65e2cd5e03f379
Kian <Ksadeghi@sas.upenn.edu>
2020-09-19 10:15:20 -0400

    Constructor completed

---
+++ node.rb
@@ -0,0 +1,14 @@
+class Node
+
+  attr_accessor :l_tree, :value, :r_tree
+
+  def initialize(l_tree, value, r_tree)
+    @l_tree = l_tree
+    @value = value
+    @r_tree = r_tree
+  end
+
+  
+
+
+end
