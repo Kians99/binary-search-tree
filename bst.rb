2c3e8a3c8708293a0390c6adb85aae37db8afd70
Kian <Ksadeghi@sas.upenn.edu>
2020-09-19 10:17:12 -0400

    Fixed syntax errors

--- bst.rb
+++ bst.rb
@@ -1,1 +1,21 @@
-asdf
+class BST
+
+  def initialize
+    @root = nil
+  end
+
+  def build_tree(array)
+    return if array.nil? || !array.instance_of(Array)
+    array.sort!
+    array.uniq!
+
+  end
+
+  private
+
+
+end
+
+
+
+
